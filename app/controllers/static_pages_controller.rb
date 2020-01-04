89188ef993cd3d9fc9e6991bded94357ff05a0e9
Luis Geniole <luis.geniole@gmail.com>
2020-01-04 12:05:21 -0300

    finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,11 @@
-class StaticPagesController < ApplicationController
-  def home
-  end
+# frozen_string_literal: true
 
-  def help
-  end
+class StaticPagesController < ApplicationController
+  def home; end
+
+  def help; end
+
+  def about; end
+
+  def contact; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see:
+  # https://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#home'
+
   get 'static_pages/home'
   get 'static_pages/help'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  root 'application#hello'
+  get 'static_pages/about'
+  get 'static_pages/contact'
 end
