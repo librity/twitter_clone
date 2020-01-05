1857f1ae3f691f6747ae2bfa5a4f12f97ff98a64
Luis Geniole <luis.geniole@gmail.com>
2020-01-05 14:13:52 -0300

    Finish chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'users/new'
   # For details on the DSL available within this file, see:
   # https://guides.rubyonrails.org/routing.html
 
@@ -9,4 +10,6 @@ Rails.application.routes.draw do
   get'/help', to: 'static_pages#help' # , as: 'helf'
   get'/about', to: 'static_pages#about'
   get'/contact', to: 'static_pages#contact'
+
+  get'/signup', to: 'users#new'
 end
