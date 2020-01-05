1a9a5f0285885e9e8dcf4f127572ce6b0a49191e
Luis Geniole <luis.geniole@gmail.com>
2020-01-05 10:43:49 -0300

    Add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
+# Gloabal helper methods for the Application.
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
     base_title = 'Ruby on Rails Tutorial Sample App'
-    if page_title.empty?
-      base_title
-    else
-      "#{page_title} | #{base_title}"
-    end
+    return base_title if page_title.empty?
+
+    page_title + ' | ' + base_title
   end
 end
