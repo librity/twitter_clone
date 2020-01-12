f4342f0744110d48763e9aef7c2d1320e3377d4a
Luis Geniole <luis.geniole@gmail.com>
2020-01-12 17:27:42 -0300

    Finish signup form w/ _error_messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,23 @@ class UsersController < ApplicationController
     # debugger
   end
 
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handles a successful save w/redirect
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
