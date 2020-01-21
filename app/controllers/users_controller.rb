299f1b9b031ea57d491310a6c62552e63b9bef33
Luis Geniole <luis.geniole@gmail.com>
2020-01-20 22:15:32 -0300

    Finish authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i[edit update]
+  before_action :correct_user, only: %i[edit update]
+
   def show
     @user = User.find(params[:id])
     # debugger
@@ -21,14 +24,13 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      # handle update
+      flash[:success] = 'Profile updated.'
+      redirect_to @user
     else
       render 'edit'
     end
@@ -39,5 +41,19 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    return if logged_in?
+
+    flash[:danger] = 'Please log in.'
+    redirect_to login_url
+  end
+
+  # Confirms the current user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_url unless current_user?(@user)
   end
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -1,7 +1,38 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class UsersEditTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+  end
+
+  test 'unsuccessful edit' do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { name: '',
+                                              email: 'foo@invalid',
+                                              password: 'foo',
+                                              password_confirmation: 'bar' } }
+    assert_template 'users/edit'
+    assert_select 'div.alert-danger', 'The form contains 4 errors'
+  end
+
+  test 'successful edit' do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = 'Foo Bar'
+    email = 'foo@bar.com'
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: '',
+                                              password_confirmation: '' } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
 end
