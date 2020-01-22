a58f77ee36aff39129fb067b60edf80ab209767e
Luis Geniole <luis.geniole@gmail.com>
2020-01-21 21:26:11 -0300

    Finish User CRUD and Chapter 10

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,8 @@
 User.create!(name: 'Example User',
              email: 'example@railstutorial.org',
              password: 'foobar',
-             password_confirmation: 'foobar')
+             password_confirmation: 'foobar',
+             admin: true)
 
 # Generate a bunch of aditional users.
 99.times do |n|
