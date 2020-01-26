3a1081095712935f225ea251f2bb8b2f7d870590
Luis Geniole <luis.geniole@gmail.com>
2020-01-26 12:00:46 -0300

    Finish Micropost Feed;

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,13 +2,18 @@
 
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: %i[create destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
     @micropost.save ? handle_save_success : handle_save_failure
   end
 
-  def destroy; end
+  def destroy
+    @micropost.destroy
+    flash[:success] = 'Micropost deleted'
+    redirect_back(fallback_location: root_url)
+  end
 
   private
 
@@ -22,6 +27,12 @@ class MicropostsController < ApplicationController
   end
 
   def handle_save_failure
+    @feed_items = current_user.feed.paginate(page: params[:page])
     render 'static_pages/home'
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
   end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,10 @@
 
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.microposts.build if logged_in?
+    return unless logged_in?
+
+    @micropost = current_user.microposts.build
+    @feed_items = current_user.feed.paginate(page: params[:page])
   end
 
   def help; end
